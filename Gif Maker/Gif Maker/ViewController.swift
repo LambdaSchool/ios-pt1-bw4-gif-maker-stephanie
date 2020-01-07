7a7fdeceb1795e524077af64a1da601df585b47f
Stephanie <sbowl001@fiu.edu>
2020-01-07 13:01:58 +0800

    init xcode

---
+++ Gif Maker/Gif Maker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gif Maker
+//
+//  Created by Stephanie Bowles on 1/7/20.
+//  Copyright © 2020 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
